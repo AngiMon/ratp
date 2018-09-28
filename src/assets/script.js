0aec37a8b147afd5b6a546162772b080c501f7c0
Angélina MONATE <angelina.monate@acensi.local>
2018-09-28 15:50:36 +0200

    animation sorting

--- src/assets/script.js
+++ src/assets/script.js
@@ -19,3 +19,45 @@ function Footer()
 
 document.addEventListener("DOMContentLoaded", Footer);
 window.addEventListener("resize", Footer);
+
+
+//sort animation
+var down, up, parent, next, allD, allU;
+
+function Sort(e)
+{
+	allD = document.getElementsByClassName('down');
+	allU = document.getElementsByClassName('up');
+	down = e.getElementsByClassName('down')[0];
+	up = e.getElementsByClassName('up')[0]
+
+	Array.from(allD).forEach(function(element)
+	{
+		element.classList.add('show');
+		element.classList.remove('hide');	
+	})
+	Array.from(allU).forEach(function(element)
+	{
+		element.classList.add('hide');
+		element.classList.remove('show');
+	})
+
+	if(down != undefined)
+	{
+		if(down.classList.contains("show"))
+		{
+			down.classList.remove('show');
+			down.classList.add('hide');
+			up.classList.add('show');
+			up.classList.remove('hide');
+		}
+		else
+		{
+			down.classList.add('show');
+			down.classList.remove('hide');
+			up.classList.remove('show');
+			up.classList.add('hide');
+		}
+	}
+}
+
